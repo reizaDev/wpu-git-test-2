0ae7a5907919fe28b756925846706c462732f333
reizaDev <rikuko135@gmail.com>
2022-02-06 09:27:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,5 +11,9 @@
     <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, amet?
     </p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
